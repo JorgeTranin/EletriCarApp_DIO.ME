ee1ba127dbccc1c826210e41d659d9d9e44ccf56
Antonio Jorge Garcia Tranin <63371272+JorgeTranin@users.noreply.github.com>
2022-12-05 12:51:33 -0400

    Implementação listener imagem carro

--- app/src/main/java/com/example/eletricarapp/MainActivity.kt
+++ app/src/main/java/com/example/eletricarapp/MainActivity.kt
@@ -8,16 +8,14 @@ import com.example.eletricarapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-/*
-        binding.ivCarro1.setOnClickListener {
+
+        binding.ivCarro11.setOnClickListener {
             val intent = Intent(this, CalculoAutonomia::class.java)
             startActivity(intent)
-
-        }*/
+        }
     }
 }
